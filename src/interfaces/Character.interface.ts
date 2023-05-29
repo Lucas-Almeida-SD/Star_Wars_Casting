d5ebaa0f1a3082f7851f7648854674afc9201e6f
lucas_almeida <lucas.almdiniz@gmail.com>
2023-05-28 20:02:21 -0400

    feat: interfaces criadas
    CharacterResponse, Character

---
+++ src/interfaces/Character.interface.ts
@@ -0,0 +1,27 @@
+export interface CharacterResponse {
+  birth_year: string
+  created: Date
+  edited: Date
+  eye_color: string
+  films: string[]
+  gender: string
+  hair_color: string
+  height: string
+  homeworld: string
+  mass: string
+  name: string
+  skin_color: string
+  species: string[]
+  starships: string[]
+  url: string
+  vehicles: string[]
+}
+
+export interface Character {
+  name: string
+  birth: string
+  homeworld: string
+  specie: string
+  height: string
+  vehicles: string[]
+}
